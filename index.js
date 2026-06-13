82a0984cfb8d6a8a299a4546b5e715b0be1ef5e4
rahulSidDev <rahulsiddhartha@protonmail.com>
2026-06-13 13:18:54 +0800

    changed cors url

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cookieParser())
 const cors = require("cors");
 
 const corsOptions = {
-  origin: 'http://localhost:5173', // frontend URL
+  origin: 'https://my-todo-app-frontend-cyan.vercel.app/', // frontend URL
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
   optionsSuccessStatus: 200,
